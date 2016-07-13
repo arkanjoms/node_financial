022f2da117c1e3e52d2702fae9b9a2b28782ff3a
Rafael Ramos de Oliveira <arkanjo.ms@gmail.com>
2016-07-13 00:31:48 -0400

    Adicionando páginas para inclusão e visualização de novo 'Customer'

--- api/controllers/CustomerController.js
+++ api/controllers/CustomerController.js
@@ -6,6 +6,52 @@
  */
 
 module.exports = {
-	
+
+  /**
+   *
+   * @param req
+   * @param res
+   */
+  'new': function (req, res) {
+    res.view();
+  },
+
+  /**
+   *
+   * @param req
+   * @param res
+   * @param next
+   */
+  create: function (req, res, next) {
+    Customer.create(req.params.all(), function customerCreated(err, customer) {
+      if (err) {
+        return next(err);
+      }
+
+      res.redirect('/customer/show/' + customer.id);
+    })
+  },
+
+  /**
+   *
+   * @param req
+   * @param res
+   * @param next
+   */
+  show: function (req, res, next) {
+    Customer.findOne(req.param('id'), function foundCustomer(err, customer) {
+      if (err) {
+        return next(err);
+      }
+
+      if (!customer) {
+        return next();
+      }
+
+      res.view({
+        customer: customer
+      })
+    })
+  }
 };
 
