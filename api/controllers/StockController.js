f11e1a3854f6e55587371c44528c40ee3379447b
Rafael Ramos de Oliveira <arkanjo.ms@gmail.com>
2016-07-15 00:31:38 -0400

    Adicionando model stock

---
+++ api/controllers/StockController.js
@@ -0,0 +1,42 @@
+/**
+ * StockController
+ *
+ * @description :: Server-side logic for managing stocks
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+
+  /**
+   *
+   * @param req
+   * @param res
+   * @param next
+   */
+  'new': function (req, res, next) {
+    Customer.findOne(req.param('owner'), function foundCustomer(err, customer) {
+      if (err) {
+        return next(err);
+      }
+
+      if (!customer) {
+        return next();
+      }
+
+      res.view({
+        customer: customer
+      })
+    })
+  },
+
+  create: function (req, res, next) {
+    Stock.create(req.params.all(), function stockCreated(err, stock) {
+      if (err) {
+        next(err);
+      }
+
+      res.json(stock);
+    })
+  }
+};
+
